1eb8493a8148ce5f9ad26a70e339c0f4facd6809
FrostNiles <opravil.jiri@email.cz>
2024-05-19 10:12:56 +0200

    commit last uncommited changes, make readme

---
+++ Readme.md
@@ -0,0 +1,22 @@
+### Testing CEC benchmark 2017 MATLAB version of code 
+
+Testing CEC benchmark 2017 from https://github.com/P-N-Suganthan/CEC2017-BoundContrained 
+
+The codes could be found in https://github.com/P-N-Suganthan/CEC2017-BoundContrained/blob/master/codes.rar 
+
+The raw codes were taken there and modified for testing purposes.
+
+### Running the tests
+
+To run tests check combine-dim-func.py where you set number of functions i, dimensions j, and number of element k.
+
+It would be required to run with matlab engine which is compatible currently with python 3.11, check documentation at https://www.mathworks.com/support/requirements/python-compatibility.html
+
+For manual testing check run-tests.py. It is possilbe to run this script with arguments number of function, dimension and number of element.
+
+### Results
+
+Results are find in test_data/result
+
+For graphs and comparisons check C version of the code at https://github.com/FrostNiles/CEC-benchmark-testing-2017-C
+
--- anomaly-deviation.py
+++ anomaly-deviation.py
@@ -65,26 +65,22 @@ sevenDigits = after[:7]
 lastTwoDigits = after.split('\'')[0][-2:]
 lastDeviation = str(deviation)
 
+#import original data from input_data/shift_data_{argNum}.txt
+with open(f'input_data/shift_data_{argNum}.txt', 'r') as file:
+     full_data = file.read()
+
+
+
+
 if int(sevenDigits) > 0:
 
     while int(sevenDigits) != 0:
-        # I have this number 4.881784197001252e-16 in deviation and it is float and I want to subtract 1 from it (I mean like 3.881784197001252e-16)
-        # I want to convert it to string and then I want to subtract 1 from it
-        backup_deviation = deviation
-        deviation = str(deviation)
-        deviation = deviation.split('.')
-        #before the floating point
-        deviation_before = deviation[0]
-        #after the floating point
-        deviation_after = deviation[1]
-        deviation[0] = str(int(deviation_before) - 1)
-        deviation[1] = deviation_after
-        deviation = '.'.join(deviation)
-        deviation = float(deviation)
         
         original_numbers = [float(i) for i in original_data.split()]
+        full_data_numbers = [float(i) for i in full_data.split()]
 
-        original_numbers[number_of_element] = original_numbers[number_of_element] - deviation
+
+        original_numbers[number_of_element] = (original_numbers[number_of_element] + full_data_numbers[number_of_element])/2
 
         with open(f'test_data/shift_data_{argNum}.txt', 'w') as file:
             for number in original_numbers:
@@ -114,155 +110,7 @@ if int(sevenDigits) > 0:
         sevenDigits = after[:7]
         lastTwoDigits = after.split('\'')[0][-2:]
         lastDeviation = str(deviation)
-        
-else:
-    if int(lastTwoDigits) < 96:
-        while int(sevenDigits) == 0 and int(lastTwoDigits) < 96:
-            backup_deviation = deviation
-            deviation = str(deviation)
-            deviation = deviation.split('.')
-            #before the floating point
-            deviation_before = deviation[0]
-            #after the floating point
-            deviation_after = deviation[1]
-            deviation[0] = str(int(deviation_before) + 1)
-            deviation[1] = deviation_after
-            deviation = '.'.join(deviation)
-            deviation = float(deviation)
-            
-            
-            original_numbers = [float(i) for i in original_data.split()]
-
-            original_numbers[number_of_element] = original_numbers[number_of_element] - deviation
-
-            with open(f'test_data/shift_data_{argNum}.txt', 'w') as file:
-                for number in original_numbers:
-                    file.write(f"{number} ")
-
-
-            run_main(eng)
-
-            with open(f'test_data/current_result_{argNum}.txt', 'r') as file:
-                result = file.read()
-
-            # Find the number in the string
-            result = re.findall(r'\d+\.\d+', result)
-
-            # Keep the number as a string to preserve trailing zeroes
-            number_string = result[0]
-            # Convert to float when you need to do a numerical operation
-
-            result = str(result)
-            result = result.split('.')
-            #before the floating point
-            before = result[0]
-            #after the floating point
-            after = result[1]
-
-            # Now I want to get the first 8 digits from after
-            sevenDigits = after[:7]
-            lastTwoDigits = after.split('\'')[0][-2:]
-            
-
-middle = (backup_deviation + deviation) / 2
-original_numbers = [float(i) for i in original_data.split()]
- 
-original_numbers[number_of_element] = original_numbers[number_of_element] - middle
- 
-with open(f'test_data/shift_data_{argNum}.txt', 'w') as file:
-    for number in original_numbers:
-        file.write(f"{number} ")
-
-
-run_main(eng)
-
-with open(f'test_data/current_result_{argNum}.txt', 'r') as file:
-        result = file.read()
-
-# Find the number in the string
-result = re.findall(r'\d+\.\d+', result)
-
-# Keep the number as a string to preserve trailing zeroes
-number_string = result[0]
-# Convert to float when you need to do a numerical operation
-
-result = str(result)
-result = result.split('.')
-#before the floating point
-before = result[0]
-#after the floating point
-after = result[1]
-
-# Now I want to get the first 8 digits from after
-sevenDigits = after[:7]
-
-lastTwoDigits = after.split('\'')[0][-2:]
-
-counter = 0
-
-while int(sevenDigits) != 0 or int(lastTwoDigits) < 96:
-    if int(sevenDigits) != 0:
-        backup_deviation = middle
-    else:
-        if int(lastTwoDigits) < 96:
-            deviation = middle
-        else:
-            print("sevenDigits is equal to 0 and lastTwoDigits is greater than 96")
-    middle = (backup_deviation + deviation) / 2
-    original_numbers = [float(i) for i in original_data.split()]
-
-    original_numbers[number_of_element] = original_numbers[number_of_element] - middle
- 
-    with open(f'test_data/shift_data_{argNum}.txt', 'w') as file:
-        for number in original_numbers:
-            file.write(f"{number} ")
-
-    
-
-    with open(f'test_data/current_result_{argNum}.txt', 'r') as file:
-            result = file.read()
-
-    # Find the number in the string
-    result = re.findall(r'\d+\.\d+', result)
-
-    # Keep the number as a string to preserve trailing zeroes
-    number_string = result[0]
-    # Convert to float when you need to do a numerical operation
-
-    result = str(result)
-    result = result.split('.')
-    #before the floating point
-    before = result[0]
-    #after the floating point
-    after = result[1]
-    counter += 1
-
-    # Now I want to get the first 8 digits from after
-    sevenDigits = after[:7]
-    lastTwoDigits = after.split('\'')[0][-2:]
-    
-    if deviation == backup_deviation:
-        deviation = lastDeviation
-        with open(f'test_data/result/result_data_{argNum}_dim_{dimension}_number_of_element_{number_of_element+1}.txt', 'w') as file:
-            file.write("deviation:")
-            file.write(deviation)
-        raise Exception("Stopping execution of anomaly-deviation.py because the deviation is the same as the backup deviation.")
-    if int(sevenDigits) == 0:
-        lastDeviation = str(middle)
-    if int(sevenDigits) == 0 and counter >= 50:
-        deviation = str(middle)
-        with open(f'test_data/result/result_data_{argNum}_dim_{dimension}_number_of_element_{number_of_element+1}.txt', 'w') as file:
-            file.write("deviation:")
-            file.write(deviation)
-        raise Exception("Stopping execution of anomaly-deviation.py because the deviation is the same as the backup deviation.")
-    elif counter >= 100:
-        deviation = lastDeviation
-        with open(f'test_data/result/result_data_{argNum}_dim_{dimension}_number_of_element_{number_of_element+1}.txt', 'w') as file:
-            file.write("deviation:")
-            file.write(deviation)
-        raise Exception("Stopping execution of anomaly-deviation.py because the counter is greater than or equal to 100.")
-
-deviation = str(middle)
 with open(f'test_data/result/result_data_{argNum}_dim_{dimension}_number_of_element_{number_of_element+1}.txt', 'w') as file:
             file.write("deviation:")
-            file.write(deviation)
+            file.write(lastDeviation)
+raise Exception("Stopping execution of anomaly-deviation.py because the deviation is less than 10e-08.")
